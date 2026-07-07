443aa228f203d57b93e4e924c8ca4e4730160b02
jihyun808 <jihyun8k@gmail.com>
2026-07-07 18:37:23 +0900

    first commit

---
+++ frontend/eslint.config.js
@@ -0,0 +1,25 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import prettier from 'eslint-config-prettier';
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+      // prettier와 충돌하는 포맷 규칙 끄기 (항상 마지막에 위치)
+      prettier,
+    ],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+]);
